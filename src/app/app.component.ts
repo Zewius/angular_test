5d55ec528b7ba4255632ea7ba1334de2c5919c3f
Zewius <crasilov.serezha@yandex.ru>
2022-07-26 17:48:52 +0500

    Added basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {latLng, MapOptions, tileLayer, Map} from "leaflet";
+
+interface City {
+  name: string;
+  coordinate: [number, number];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularstart';
+
+export class AppComponent implements OnInit {
+
+  title = 'angular start';
+
+  map!: Map;
+  mapOptions!: MapOptions;
+
+  coordinateBySelectedCity: [number, number] = [55.7522, 37.6156];
+
+  cities: City[] = [
+    {name: "Москва", coordinate: [55.7522, 37.6156]},
+    {name: "Санкт-Петербург", coordinate: [59.9386, 30.3141]},
+    {name: "Новосибирск", coordinate: [55.0415, 82.9346]},
+    {name: "Екатеринбург", coordinate: [56.8519, 60.6122]},
+    {name: "Казань", coordinate: [55.7887, 49.1221]}
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initMapOptions();
+  }
+
+  onReadyMap(map: Map): void {
+    this.map = map;
+  }
+
+  showSelectedCity(): void {
+    this.map?.setView(this.coordinateBySelectedCity);
+  }
+
+  private initMapOptions(): void {
+    this.mapOptions = {
+      center: this.cities[0].coordinate,
+      zoom: 12,
+      layers: [
+        tileLayer(
+          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {maxZoom: 18, attribution: 'Map data © OpenStreetMap contributors'
+          })
+      ],
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
